e29f70bb78badf66607cff346404ba6b4a30d836
foxy82 <foxy82.github@gmail.com>
2022-01-25 09:42:38 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
-# Set required settings and modules
+#!/bin/bash
 
+# Set required settings and modules
 echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
 echo "dwc2" | sudo tee -a /etc/modules
 echo "libcomposite" | sudo tee -a /etc/modules
 
-sudo cp pi_zero_usb_media_remote /usr/bin/pi_zero_usb_media_remote
+# Install the USB HID files
+sudo cp ./pi_zero_usb_media_remote /usr/bin/pi_zero_usb_media_remote
 sudo chmod +x /usr/bin/pi_zero_usb_media_remote
+
+# Set the USB HID file to run on startup
+sudo cp ./usb_media_remote.service /lib/systemd/system/usb_media_remote.service
+sudo systemctl daemon-reload
+sudo systemctl enable usb_media_remote.service
