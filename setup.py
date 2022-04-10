20e3e0b995cb25f2b8e2819bc01ad10bac956b12
Foxy82 <foxy82.github@gmail.com>
2022-04-10 09:21:16 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
  
 version = re.search(
     '^__version__\s*=\s*"(.*)"',
-    open('pi_media_control/__init__.py').read(),
+    open('pi_media_remote/__init__.py').read(),
     re.M
     ).group(1)
 
@@ -20,17 +20,18 @@ with open("README.md", "rb") as f:
  
  
 setup(
-    name = "pi_media_control",
-    packages = ["pi_media_control"],
+    name = "pi_media_remote",
+    packages = ["pi_media_remote"],
     entry_points = {
             "console_scripts": [
                 # TODO consolidate this into one app
                 'UsbHidRest = pi_media_remote.UsbHidRest:main',
                 'UsbHidServer = pi_media_remote.UsbHidServer:main',
+                'send_key = pi_media_remote.send_key:main',
             ]
         },
     version = version,
-    description = "Application to take commands and send via USB to a MEdia device like a FireTV or Andriod TV with ChromeCast",
+    description = "Application to take commands and send via USB to a Media device like a FireTV or Andriod TV with ChromeCast",
     long_description = long_descr,
     author = "Foxy82",
     author_email = "foxy82.github@gmail.com",
