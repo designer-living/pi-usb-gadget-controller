2a805e1af874d611f020e086fb24788d8c9e58da
foxy82 <foxy82.github@gmail.com>
2022-04-11 16:30:10 +0100

    Make ready for pypi

--- setup.py
+++ setup.py
@@ -29,12 +29,25 @@ setup(
             ]
         },
     version = version,
+    license='Apache 2.0'
     description = "Application to take commands and send via USB to a Media device like a FireTV or Andriod TV with ChromeCast",
     long_description = long_descr,
     author = "Foxy82",
     author_email = "foxy82.github@gmail.com",
     url = "https://github.com/foxy82/pi-usb-gadget-controller",
+    download_url = f'https://github.com/foxy82/pi-usb-gadget-controller/{version}.tar.gz',
     install_requires = [
             'aiohttp',
         ],
-    )
+    classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: Apache Software License',   # Again, pick a license
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],        
+)
