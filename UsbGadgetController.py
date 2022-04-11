9f59252298ee499156f602c75e2fb4af7fd957a2
foxy82 <foxy82.github@gmail.com>
2022-04-11 10:27:48 +0100

    Set logging when running from helper script

--- UsbGadgetController.py
+++ UsbGadgetController.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
- 
- 
+
+
 """Convenience wrapper for running UsbGadgetController directly from source tree."""
- 
- 
+
+import logging 
 from pi_media_remote.UsbGadgetController import main
- 
- 
+
+
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     main()
--- UsbHidServer.py
+++ UsbHidServer.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
- 
- 
+
+
 """Convenience wrapper for running UsbHidServer directly from source tree."""
- 
- 
+
+import logging
 from pi_media_remote.UsbHidServer import main
- 
- 
+
+
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     main()
