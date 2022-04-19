bc2a988a38bb135a0e7958ca11d506c30f6060c0
Foxy82 <foxy82.github@gmail.com>
2022-04-19 07:11:32 +0100

    Add logging to update script

--- update.sh
+++ update.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # Uncomment if you need to install requirements
+echo "Stopping Service"
 sudo service usb_gadget_controller stop
+echo "Service stopped"
+echo "Updating software"
 sudo pip install --no-deps --force-reinstall .
+echo "Software updated"
+echo "Starting service"
 sudo service usb_gadget_controller start
+echo "Service Started"
